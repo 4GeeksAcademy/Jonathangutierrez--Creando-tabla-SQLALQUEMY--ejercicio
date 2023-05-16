b08116141002f56553c94c37bc20de0d4d181148
Jonathan Gutiérrez <120731494+Goodtierrez@users.noreply.github.com>
2023-05-16 22:09:40 +0000

    4geeks students projects completed

--- src/app.py
+++ src/app.py
@@ -8,7 +8,8 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Student, Project, Submission
+from datetime import datetime
 #from models import Person
 
 app = Flask(__name__)
@@ -45,7 +46,100 @@ def handle_hello():
 
     return jsonify(response_body), 200
 
+
+# FUNCIONES QUE VAMOS A IR UTILIZANDO EN with app.app_contexty():
+
+
+# Creo estudiante en la tabla
+def create_student():
+    student = Student(name_student="Alex", email="alex@gmail.es", programming_skills=True) 
+    # encolo el producto que quiero crear en base de datos
+    db.session.add(student)
+    # Confirmo los cambios para que haga los INSERT
+    db.session.commit()
+
+# Recuperar 1 estudiante
+def get_student(id):
+    student = Student.query.get(id)
+    print(student)
+
+# Recuperar a todos los estudientes
+def get_all_students():
+    student = Student.query.all()
+    for s in student:
+        print(s)   
+
+# Filtar por campos. Por ejemplo: El estudiante con el ID más alto
+def student_id_higher():
+    student = Student.query.order_by(Student.id.desc()).first()
+    print(student)
+
+# Filtrar por campos. Por ejemplo: los estudiantes con conocimientos de programación previa
+def students_with_programming_skills():
+    students = Student.query.filter(Student.programming_skills == True).all()
+    for student in students:
+        print(student)
+
+# Crear un nuevo proyecto en la tabla Project
+def create_project():
+    project = Project(project_name="Fetch API", topics="JavaScript, Fetch, DOM")
+    db.session.add(project)
+    db.session.commit()
+
+# Recuperar todos los proyectos y mostrarlos en el terminal
+def get_all_projects():
+    projects = Project.query.all()
+    for project in projects:
+        print(project) 
+
+# Recupera todos los proyectos que tratan sobre el topic “JavaScript”.
+def get_all_projects_with_topic(topic):
+    projects = Project.query.filter(Project.topics.like(f'%{topic}%')).all()
+    print(projects)
+
+# Estudiantes entregan proyectos
+def delivered_project(s_id, p_id, date):
+    # student_id es el nombre de la columna de submission
+    # s_id simplemente es el valor del parámetro de la función, abreviatura de student_id. Valdría igual sin abreviar (student_id = student_id) pero lo pongo así para acordarme de donde viene.
+    projects_done = Submission(student_id = s_id, project_id = p_id, submited_date=date) 
+    db.session.add(projects_done)
+    db.session.commit() 
+
+# Recupera todos los proyectos entregados por un studiante (usa su Id para recuperar el estudiante de la base de datos). Para cada proyecto que ha entregado, muestra su nombre y sus topics.
+def get_projects_delivered_by_student(s_id):
+    student = Student.query.get(s_id)
+    for project in student.submissions:
+        print(project)
+
+#  Recupera todos los proyectos de todos lo estudiantes cuya fecha de entrega ha sido antes del 17 de Abril de 2023.
+#  Para cada proyecto, muestra el nombre del estudiante que lo entregó y la fecha exacta de entrega.
+
+def get_projects_before_date(date):
+    submissions = Submission.query.filter(Submission.submited_date < date).all()
+    for submission in submissions:
+        print(submission)
+
+
+
+
+
+
 # this only runs if `$ python src/app.py` is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
-    app.run(host='0.0.0.0', port=PORT, debug=False)
+   # app.run(host='0.0.0.0', port=PORT, debug=False)
+
+    # Lo necesitamos para pdoer ejecutar código Python normal fuera del entorno de Flask
+    with app.app_context():
+        # create_student()
+        # get_student(3)
+        # get_all_students()
+        # student_id_higher()
+        # students_with_programming_skills()
+        # create_project()
+        # get_all_projects()
+        # get_all_projects_with_topic("JavaScript")
+        # delivered_project(3, 2, datetime(2023, 4, 18))
+        # get_projects_delivered_by_student(3)
+        get_projects_before_date(datetime(2023, 4, 17))
+
--- src/models.py
+++ src/models.py
@@ -1,4 +1,5 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy.orm import relationship
 
 db = SQLAlchemy()
 
@@ -23,4 +24,36 @@ class Student(db.Model):
     name_student = db.Column(db.String(120), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     programming_skills = db.Column(db.Boolean(), unique=False, nullable=False)
+
+    
+
+
+    # En la función repr le decimos cómo debemos representar en pantalla los datos que nos piden. Self se refiere a si mismo, en la tabla Student. Podemos añadir todos los datos que queramos mostrar.       
+    def __repr__(self):
+        return f'<Student {self.name_student} - {self.email}>'
+    
+class Project(db.Model):
+     id = db.Column(db.Integer, primary_key=True)
+     project_name = db.Column(db.String(120), unique=True, nullable=False)   
+     topics = db.Column(db.String(120), unique=False, nullable=False)  
+
+     def __repr__(self):
+         return f'<Project {self.project_name}>'
+      
+class Submission(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    student_id = db.Column(db.Integer, db.ForeignKey('student.id'), nullable=False)
+    project_id = db.Column(db.Integer, db.ForeignKey('project.id'), nullable=False)
+    submited_date = db.Column(db.Date, nullable=False)
+
+
+     # En backref ponemos el nombre de la tabla, en minúsculas y en plural
+    student = relationship('Student', backref='submissions')
+    project = relationship('Project', backref='submissions')
+
+    def __repr__(self):
+        return f'{self.student.name_student} - <Project {self.submited_date}>'
+
+   
+
 
